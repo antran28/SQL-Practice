eceb2482b2d7ec461bf67118318d887b5f23c028
Ann Tran <102011433+antran28@users.noreply.github.com>
2023-07-25 15:01:02 +1000

    Create Placements.sql

---
+++ Placements.sql
@@ -0,0 +1,7 @@
+SELECT s.Name
+FROM Students s
+JOIN Friends f ON s.ID = f.ID
+JOIN Packages sp ON s.ID = sp.ID
+JOIN Packages fp ON f.Friend_ID = fp.ID
+WHERE fp.Salary > sp.Salary
+ORDER BY fp.Salary;
